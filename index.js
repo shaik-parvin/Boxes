d56a0b4823efe9f52e6a22c2f3fb15559687167e
shaikparvin <shaikparvin807@gmail.com>
2024-01-07 07:11:40 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const Box = props => {
+  //  Write your code here.
+  const {className, text} = props
+  return (
+    <div className={`box ${className}`}>
+      <p className='title'>{text}</p>
+    </div>
+  )
+}
+
+const element = (
+  //  Write your code here.
+  <div className='bg-container'>
+    <h1 class='heading'>Boxes</h1>
+    <div className='card'>
+      <Box className='small-box' text='Small' />
+      <Box className='medium-box' text='Medium' />
+      <Box className='large-box' text='Large' />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
